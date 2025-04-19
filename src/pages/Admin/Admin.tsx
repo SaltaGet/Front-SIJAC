f79d873ca4b81259a7f463466378658429beb8ab
PicoHBK <picohbk322@gmail.com>
2025-04-18 23:24:40 -0300

    fix

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -5,28 +5,55 @@ import FormCreatePost from "./FormCreatePost";
 import AdimEditPost from "./AdminEditBlog";
 import useAuthStore from "@/store/useAuthStore";
 import { useNavigate } from "react-router-dom";
+import { Menu } from "lucide-react";
 
 const Admin = () => {
-  const [activeSection, setActiveSection] = useState<"selector" | "appointment" | "createPost" | "editPost">("selector");
-  const {token} = useAuthStore()
-  const navigate = useNavigate()
-  const {clearAuth} = useAuthStore()
+  const [activeSection, setActiveSection] = useState<
+    "selector" | "appointment" | "createPost" | "editPost"
+  >("selector");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { token, clearAuth } = useAuthStore();
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (!token) {
-      navigate("/login")
+      navigate("/login");
     }
+  }, [navigate, token]);
 
-  },[navigate,token])
+  const handleLogout = () => {
+    clearAuth();
+    navigate("/login");
+  };
 
   return (
     <div className="min-h-screen w-full flex flex-col md:flex-row">
+      {/* Botón Hamburguesa */}
+      <div className="flex justify-between items-center md:hidden p-4 bg-prim-100 border-b">
+        <h2 className="text-lg font-semibold text-prim-700">Admin Panel</h2>
+        <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+          <Menu className="text-prim-700 w-6 h-6" />
+        </button>
+      </div>
+
       {/* Barra lateral */}
-      <aside className="w-full md:w-64 bg-prim-100 border-b md:border-b-0 md:border-r p-4">
-        <h2 className="text-lg font-semibold text-prim-700 mb-4">Admin Panel</h2>
-        <nav className="flex md:flex-col gap-2">
+      <aside
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } md:block w-full md:w-64 bg-prim-100 border-b md:border-b-0 md:border-r p-4`}
+      >
+        <h2 className="text-lg font-semibold text-prim-700 mb-4 hidden md:block">
+          Admin Panel
+        </h2>
+
+        {/* nav en fila para mobile, columna para desktop */}
+        <nav className="flex flex-col gap-2 h-full">
           <button
-            onClick={() => setActiveSection("selector")}
-            className={`text-left p-2 rounded ${
+            onClick={() => {
+              setActiveSection("selector");
+              setIsMenuOpen(false);
+            }}
+            className={`p-2 rounded ${
               activeSection === "selector"
                 ? "bg-prim-500 text-white"
                 : "hover:bg-prim-200 text-prim-800"
@@ -35,8 +62,11 @@ const Admin = () => {
             Crear Turnos
           </button>
           <button
-            onClick={() => setActiveSection("appointment")}
-            className={`text-left p-2 rounded ${
+            onClick={() => {
+              setActiveSection("appointment");
+              setIsMenuOpen(false);
+            }}
+            className={`p-2 rounded ${
               activeSection === "appointment"
                 ? "bg-prim-500 text-white"
                 : "hover:bg-prim-200 text-prim-800"
@@ -45,8 +75,11 @@ const Admin = () => {
             Turnos
           </button>
           <button
-            onClick={() => setActiveSection("createPost")}
-            className={`text-left p-2 rounded ${
+            onClick={() => {
+              setActiveSection("createPost");
+              setIsMenuOpen(false);
+            }}
+            className={`p-2 rounded ${
               activeSection === "createPost"
                 ? "bg-prim-500 text-white"
                 : "hover:bg-prim-200 text-prim-800"
@@ -55,8 +88,11 @@ const Admin = () => {
             Crear Post
           </button>
           <button
-            onClick={() => setActiveSection("editPost")}
-            className={`text-left p-2 rounded ${
+            onClick={() => {
+              setActiveSection("editPost");
+              setIsMenuOpen(false);
+            }}
+            className={`p-2 rounded ${
               activeSection === "editPost"
                 ? "bg-prim-500 text-white"
                 : "hover:bg-prim-200 text-prim-800"
@@ -65,17 +101,8 @@ const Admin = () => {
             Tus Posteos
           </button>
           <button
-            onClick={()=> {
-              clearAuth();
-              navigate("login")
-
-            }
-            }
-            className={`text-left p-2 rounded ${
-              activeSection === "editPost"
-                ? "bg-prim-500 text-white"
-                : "hover:bg-prim-200 text-prim-800"
-            }`}
+            onClick={handleLogout}
+            className="p-2 rounded bg-red-500 text-white font-semibold mt-8 "
           >
             Cerrar Sesión
           </button>
